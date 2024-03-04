c3aee60248219df8ebbe6982dee679d014e635ff
sramazzina <sergio.ramazzina@serasoft.it>
2024-03-04 21:09:13 +0100

    First implementation using the new library to implement with Azure Storage support

--- plugins/tech/azure/src/main/java/org/apache/hop/vfs/azure/AzureVfsPlugin.java
+++ plugins/tech/azure/src/main/java/org/apache/hop/vfs/azure/AzureVfsPlugin.java
@@ -18,20 +18,50 @@
 
 package org.apache.hop.vfs.azure;
 
+import com.dalet.vfs2.provider.azure.AzFileSystemConfigBuilder;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.commons.vfs2.FileSystemException;
+import org.apache.commons.vfs2.UserAuthenticationData;
+import org.apache.commons.vfs2.UserAuthenticator;
+import org.apache.commons.vfs2.auth.StaticUserAuthenticator;
 import org.apache.commons.vfs2.provider.FileProvider;
+import org.apache.commons.vfs2.util.UserAuthenticatorUtils;
+import org.apache.hop.core.variables.IVariables;
+import org.apache.hop.core.variables.Variables;
 import org.apache.hop.core.vfs.plugin.IVfs;
 import org.apache.hop.core.vfs.plugin.VfsPlugin;
+import com.dalet.vfs2.provider.azure.AzFileProvider;
+import org.apache.hop.vfs.azure.config.AzureConfig;
+import org.apache.hop.vfs.azure.config.AzureConfigSingleton;
 
 @VfsPlugin(type = "azure", typeDescription = "Azure VFS plugin")
 public class AzureVfsPlugin implements IVfs {
+  private Log logger = LogFactory.getLog(AzureVfsPlugin.class);
   @Override
   public String[] getUrlSchemes() {
-    return new String[] {"azure"};
+    return new String[] {"azbs"};
   }
 
   @Override
   public FileProvider getProvider() {
-    AzureFileProvider fileProvider = new AzureFileProvider();
-    return fileProvider;
+
+    logger.info("Initialize Azure client");
+
+    IVariables variables = Variables.getADefaultVariableSpace();
+    AzureConfig config = AzureConfigSingleton.getConfig();
+    String account = variables.resolve(config.getAccount());
+    String key = variables.resolve(config.getKey());
+
+    AzFileProvider azfp = new AzFileProvider();
+
+    StaticUserAuthenticator auth = new StaticUserAuthenticator("", account, key);
+      try {
+          AzFileSystemConfigBuilder.getInstance().setUserAuthenticator(azfp.getDefaultFileSystemOptions(), auth);
+      } catch (FileSystemException e) {
+          throw new RuntimeException(e);
+      }
+
+      return azfp;
   }
 }
